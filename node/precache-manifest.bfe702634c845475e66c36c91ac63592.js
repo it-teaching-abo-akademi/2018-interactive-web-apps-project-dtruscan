1458bee09d456424cfd10ad06dfa70a1ddff5754
Dragos Truscan <dragos.truscan@abo.fi>
2018-12-05 18:13:57 +0200

    Add files via upload

---
+++ node/precache-manifest.bfe702634c845475e66c36c91ac63592.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "970ae212a25ed93262e1",
+    "url": "/static/js/main.970ae212.chunk.js"
+  },
+  {
+    "revision": "f4e17ff37feaffb129fc",
+    "url": "/static/js/1.f4e17ff3.chunk.js"
+  },
+  {
+    "revision": "970ae212a25ed93262e1",
+    "url": "/static/css/main.59dbfb60.chunk.css"
+  },
+  {
+    "revision": "c548bd666061f1b69ab5aea87b68cb62",
+    "url": "/index.html"
+  }
+];
